55eef02154aa00036742411a6cedb8a0836b26b7
Marta Kwiatkowska <marta.kwiatkowska@sybillatechnologies.com>
2019-05-10 23:52:59 +0200

    added Neuron class without any methods

--- Assets/Scripts/Neural Network/Neuron.cs
+++ Assets/Scripts/Neural Network/Neuron.cs
@@ -2,7 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Neuron : MonoBehaviour
+namespace Assets.Scripts.Neural_Network
 {
-    
+    public class Neuron : MonoBehaviour
+    {
+        private List<Synapse> _inputSynapses;
+        private List<Synapse> _outputSynapses;
+        //private double Bias;
+
+        System.Random random = new System.Random();
+        public Neuron()
+        {
+            _inputSynapses = new List<Synapse>();
+            _outputSynapses = new List<Synapse>();
+            //Bias = random.NextDouble();
+        }
+    }
 }
