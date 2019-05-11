c9129414d9c22d887542bd9896cd312aff66b883
Marta Kwiatkowska <marta.kwiatkowska@sybillatechnologies.com>
2019-05-11 22:21:49 +0200

    added class Neuron

--- Assets/Scripts/Neural Network/Neuron.cs
+++ Assets/Scripts/Neural Network/Neuron.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class Neuron : MonoBehaviour
 {
-   
+    public List<Synapse> InputSynapses { get; private set; }
+    public List<Synapse> OutputSynapses { get; private set; }
+    public double Value { get; private set; }
+
+    public Neuron()
+    {
+        InputSynapses = new List<Synapse>();
+        OutputSynapses = new List<Synapse>();
+    }
 }
+        
