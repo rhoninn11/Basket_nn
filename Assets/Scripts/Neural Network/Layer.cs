eb474226e555c230d8ec008b3641c86291d0c94b
Marta Kwiatkowska <marta.kwiatkowska@sybillatechnologies.com>
2019-05-11 22:27:30 +0200

    added class Layer

--- Assets/Scripts/Neural Network/Layer.cs
+++ Assets/Scripts/Neural Network/Layer.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class Layer : MonoBehaviour
 {
-   
+    public string Name { get; private set; }
+    public List<Neuron> Neurons { get; private set; }
+    public double Weight { get; private set; }
+
+    public Layer(string name, int size, double initialWeight)
+    {
+        Name = name; 
+
+        SetNeuronsCount(size);
+
+        Weight = initialWeight;
+    }
+
+    private void SetNeuronsCount(int size)
+    {
+        Neurons = new List<Neuron>();
+        for (int i = 0; i < size; i++)
+        {
+            var neuron = new Neuron();
+            Neurons.Add(neuron);
+        }
+    }
 }
