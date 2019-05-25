820a5901ce9453cbb29f20c3f0c45c2759a8c6c9
Marta Kwiatkowska <marta.kwiatkowska@sybillatechnologies.com>
2019-05-26 00:02:49 +0200

    simple script for UI data setting

---
+++ Assets/Scripts/UIDataSetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDataSetter : MonoBehaviour
+{
+    [Header("Player Settings")]
+    public Text Position;
+    public InputField Force;
+
+    [SerializeField] GameObject Player;
+
+    //[Header("Naural Network Settings")]
+
+    private void Update()
+    {
+        SetPlayerData();
+    }
+
+    private void SetPlayerData()
+    {
+        var posX = Player.transform.localPosition.x;
+        var posY = Player.transform.localPosition.y;
+        var posZ = Player.transform.localPosition.z;
+        Position.text = $"Position: X: {posX:00.00}, Y: {posY:00.00}, Z: {posZ:00.00}";
+    }
+}
