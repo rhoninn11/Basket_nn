4e5dfd90ed58e4fdf15e2ef38f325c2e6f83fccc
Marta Kwiatkowska <marta.kwiatkowska@sybillatechnologies.com>
2019-05-28 22:50:18 +0200

    changing camera mode with M button

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -9,18 +9,28 @@ public class CameraMovement : MonoBehaviour
     private float rotationX = 0.0f;
     private float rotationY = 0.0f;
 
+    private bool IsMoving = true;
+
 
     void Update()
     {
-        rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
-        rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
-        rotationY = Mathf.Clamp(rotationY, -90, 90);
+        if (IsMoving)
+        {
+            rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+            rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+            rotationY = Mathf.Clamp(rotationY, -90, 90);
 
-        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
-        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+            transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+            transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
 
 
-        transform.position += transform.forward * movementSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.position += transform.right * movementSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+            transform.position += transform.forward * movementSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += transform.right * movementSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            IsMoving = !IsMoving;
+        }
     }
 }
